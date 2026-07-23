7366e5214977b64a594494c8ff38caedc1a8c0ba
erinner <107972715+ocelot2024@users.noreply.github.com>
2026-07-23 21:14:35 +0900

    インストール可能に

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,38 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+    plugins: [
+        vue(),
+        vueDevTools(),
+        VitePWA({
+            devOptions: { enabled: true },
+            manifest: {
+                name: "LiveSFX",
+                short_name: "LIVE",
+                icons: [
+                    {
+                        sizes: "192x192",
+                        src: "pwa-192x192.png",
+                        type: "image/png",
+                    },
+                    {
+                        sizes: "512x512",
+                        src: "pwa-512x512.png",
+                        type: "image/png",
+                    },
+                    {
+                        sizes: "512x512",
+                        src: "maskable-icon-512x512.png",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+    ],
     server: {
         host: true,
     },
