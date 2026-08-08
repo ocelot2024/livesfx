64d49b2eaf552d28e522b6bff30588adab8cec8b
erinner <107972715+ocelot2024@users.noreply.github.com>
2026-08-08 21:17:41 +0900

    アニメーションを強化

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
+import { Transition } from 'vue';
+
 defineEmits(['close'])
-const props = defineProps<{ title?: string }>()
+
+const props = defineProps<{ title?: string, show: boolean }>()
 </script>
 <template>
-    <div class="full">
-        <div class="dialog">
-            <header>
-                <div>
-                    <h3>{{ props.title }}</h3>
-                </div><button @click="$emit('close')">X</button>
-            </header>
-            <slot />
+    <Transition>
+        <div class="full" v-if="props.show">
+            <div class="dialog">
+                <header>
+                    <div>
+                        <h3>{{ props.title }}</h3>
+                    </div><button @click="$emit('close')">X</button>
+                </header>
+                <slot />
+            </div>
         </div>
-    </div>
+    </Transition>
 </template>
 <style scoped>
 .full {
@@ -55,5 +60,35 @@ header {
     background-color: var(--gray-5);
     min-width: 100px;
     min-height: 100px;
+}
+
+
+.v-enter-active,
+.v-leave-active {
+    transition: background-color .2s ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+    background-color: rgba(136, 136, 136, 0);
+}
+
+.v-enter-active .dialog,
+.v-leave-active .dialog {
+    transition: transform .2s ease-out;
+}
+
+.v-enter-from .dialog {
+    transform: scale(0.8);
+}
+
+.v-leave-to .dialog {
+    transform: scale(0.5);
+    opacity: 0;
+}
+
+.v-enter-to .dialog,
+.v-leave-from .dialog {
+    transform: scale(1);
 }
 </style>
--- src/components/Sounds.vue
+++ src/components/Sounds.vue
@@ -35,7 +35,7 @@ const onClick = async (id: string, e: MouseEvent) => {
             </button>
         </div>
 
-        <Modal v-if="selectedSound" @close="selectedSound = undefined"
+        <Modal :show="selectedSound" @close="selectedSound = undefined"
             :title="store.library.find(v => v.id == selectedSound)?.filename">
             <editor :sound-id="selectedSound" />
         </Modal>
