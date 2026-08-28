ed5b25facb6fea70a93eb3c113a9ee95319cd2c8
millai <mko.makao@outlook.jp>
2026-08-28 14:45:41 +0900

    console.log削除

--- src/core/audioEngine/sounds.ts
+++ src/core/audioEngine/sounds.ts
@@ -151,7 +151,6 @@ export class SoundLibrary {
         if (meta.type === SoundFileType.SFX || meta.type === undefined) {
             const sound = new Sound(meta, file as AudioBuffer);
             this.sounds[meta.id] = sound;
-            console.log(this.sounds);
             return meta.id;
         } else {
             console.log("Adding BGM");
@@ -223,7 +222,6 @@ export class SoundLibrary {
     get_bgm_library() {
         let frag: Record<string, SoundMeta> = {};
         for (const i in this.musics) {
-            console.log(this.musics);
             if (!this.musics[i]) continue;
             const info = this.musics[i].getInfo();
             if (info.type !== SoundFileType.BGM) continue;
